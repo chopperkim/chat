b8dd141bf18b39fffb1bfce7884f766bdd7b55ac
chopperkim <chocojhkim@gmail.com>
2020-09-13 20:29:54 +0900

    modify

--- src/test/java/kim/chopper/chat/ChatApplicationTests.java
+++ src/test/java/kim/chopper/chat/ChatApplicationTests.java
@@ -1,13 +1,18 @@
 package kim.chopper.chat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChatApplicationTests {
 
+    @Autowired
+    private ChatRepository repository;
+
     @Test
     void contextLoads() {
+        repository.save(new Chat("miso", "미소의 세상2"));
     }
 
 }
