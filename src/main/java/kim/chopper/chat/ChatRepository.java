d69270a3069883bbbb58569106c8cbc8419bc180
chopperkim <chocojhkim@gmail.com>
2020-09-17 12:03:10 +0900

    remove repository annotation

--- src/main/java/kim/chopper/chat/ChatRepository.java
+++ src/main/java/kim/chopper/chat/ChatRepository.java
@@ -3,6 +3,5 @@ package kim.chopper.chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 }
